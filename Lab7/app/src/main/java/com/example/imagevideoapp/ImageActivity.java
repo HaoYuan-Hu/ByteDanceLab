b1d0bef9d06c42fb1082207d0f3dd98afdac2571
HaoYuan-Hu <1124730278@qq.com>
2021-05-08 14:35:08 +0800

    finish lab7

--- Lab7/app/src/main/java/com/example/imagevideoapp/ImageActivity.java
+++ Lab7/app/src/main/java/com/example/imagevideoapp/ImageActivity.java
@@ -1,4 +1,70 @@
 package com.example.imagevideoapp;
 
-public class ImageActivity {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+public class ImageActivity extends AppCompatActivity {
+
+    ViewPager pager = null;
+    LayoutInflater layoutInflater = null;
+    List<View> pages = new ArrayList<View>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // 设置页面
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        layoutInflater = getLayoutInflater();
+        pager = (ViewPager) findViewById(R.id.view_pager);
+
+
+        // 添加本地图片
+        addImage(R.drawable.drawableimage);
+        addImage(R.drawable.ic_launcher_background);
+        addImage(R.drawable.ic_launcher_foreground);
+
+        // 添加网络图片
+        addImage("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01b0d857b1a34d0000012e7e87f5eb.gif&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1620748277&t=efbace1497367b361c015caff7b0cb63");
+        addImage("https://images.shobserver.com/news/690_390/2021/4/11/f7098bbcc81d48dfaf016ade4496f6f4.jpg");
+        addImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562328963756&di=9c0c6c839381c8314a3ce8e7db61deb2&imgtype=0&src=http%3A%2F%2Fpic13.nipic.com%2F20110316%2F5961966_124313527122_2.jpg");
+        addImage("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.17qq.com%2Fimg_biaoqing%2F85115267.jpeg&amp;refer=http%3A%2F%2Fwww.17qq.com&amp;app=2002&amp;size=f9999,10000&amp;q=a80&amp;n=0&amp;g=0n&amp;fmt=jpeg?sec=1622970017&amp;t=e076fbe420ebfd3a06f31809df6e3e82");
+        ViewAdapter adapter = new ViewAdapter();
+        adapter.setDatas(pages);
+        pager.setAdapter(adapter);
+    }
+
+
+    private void addImage(int resId) {
+        ImageView imageView = (ImageView) layoutInflater.inflate(R.layout.activity_image_item, null);
+        Glide.with(this)
+                .load(resId)
+                .error(R.drawable.error)
+                .into(imageView);
+        pages.add(imageView);
+    }
+
+    private void addImage(String path) {
+        ImageView imageView = (ImageView) layoutInflater.inflate(R.layout.activity_image_item, null);
+        Glide.with(this)
+                .load(path)
+                .apply(new RequestOptions().circleCrop().diskCacheStrategy(DiskCacheStrategy.ALL))
+                .error(R.drawable.error)
+                .into(imageView);
+        pages.add(imageView);
+    }
+
+
 }
--- Lab7/app/src/main/java/com/example/imagevideoapp/VideoActivity.java
+++ Lab7/app/src/main/java/com/example/imagevideoapp/VideoActivity.java
@@ -1,4 +1,178 @@
 package com.example.imagevideoapp;
 
-public class VideoActivity {
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class VideoActivity extends AppCompatActivity {
+    private String TAG = "VideoActivity";
+
+    private TextView timeMsg; // 进度条后的时间信息
+    private VideoView videoView; // 视频播放视窗
+    private ImageView restartBtn; // 重新播放按钮
+    private ImageView controlBtn; // 开始/暂停按钮
+    private SeekBar seekBar; // 进度条
+
+    private String totalTime; // 视频总时长
+    private String currentTime; // 视频当前时长
+
+    // 进度条跟随视频进度的线程
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            if (videoView.isPlaying()) {
+                int current = videoView.getCurrentPosition();
+                seekBar.setProgress(current);
+
+                Log.d(TAG,"seekBar progress:" + seekBar.getProgress());
+                Log.d(TAG,"video progress:" + current);
+
+                currentTime = getFormalTime(current);
+                timeMsg.setText(currentTime + " / " + totalTime);
+            }
+            handler.postDelayed(runnable, 500);
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // 设置页面
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        timeMsg = findViewById(R.id.video_time);
+
+        // 设置重启按钮
+        restartBtn = findViewById(R.id.restart_btn);
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (videoView != null){
+                    videoView.resume();
+                    videoView.start();
+                    controlBtn.setImageResource(R.drawable.pause);
+                }
+            }
+        });
+
+        // 设置开关按钮
+        controlBtn = findViewById(R.id.control_btn);
+        controlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 如果正在播放，则暂停，并把图片修改成开始按钮
+                if(videoView != null && videoView.isPlaying()){
+                    videoView.pause();
+                    controlBtn.setImageResource(R.drawable.start);
+                    Toast.makeText(v.getContext(),"视频暂停",Toast.LENGTH_SHORT).show();
+                }
+                // 如果正在暂停，则开始播放，并把图片修改成开始按钮
+                else if(videoView != null && !videoView.isPlaying()){
+                    videoView.start();
+                    controlBtn.setImageResource(R.drawable.pause);
+                    Toast.makeText(v.getContext(),"视频开始",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // 设置进度条
+        seekBar = findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(onSeekBarChangeListener);
+
+        // 设置视频内容
+        videoView = findViewById(R.id.videoView);
+        videoView.getHolder().setFormat(PixelFormat.TRANSPARENT);
+        videoView.setZOrderOnTop(true);
+        videoView.setVideoPath(getVideoPath(R.raw.big_buck_bunny));
+
+        // 监听准备状态
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                // 获取视频数据
+                totalTime = getFormalTime(videoView.getDuration());
+                currentTime = getFormalTime(videoView.getCurrentPosition());
+
+                timeMsg.setText(currentTime + " / " + totalTime);
+                seekBar.setMax(videoView.getDuration());
+
+                Toast.makeText(VideoActivity.this, "视频加载完毕", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 播放完毕后回调
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(VideoActivity.this, "播放结束", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 播放视频
+        handler.postDelayed(runnable, 0);
+        videoView.start();
+    }
+
+    // 设置 seekBar 的变化监听
+    private SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        // 当进度条停止修改的时候触发
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // 取得当前进度条的刻度
+            int progress = seekBar.getProgress();
+            if (videoView.isPlaying()) {
+                // 设置当前播放的位置
+                videoView.seekTo(progress);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+
+        }
+    };
+
+
+    // 标准化时间，把秒转化成分秒组合
+    private String getFormalTime(long millionSeconds) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(millionSeconds);
+        return simpleDateFormat.format(c.getTime());
+    }
+
+    // 补全视频路径
+    private String getVideoPath(int resId) {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
+    }
+
+    // 销毁函数
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+
 }
